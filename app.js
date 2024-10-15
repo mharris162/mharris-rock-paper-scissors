5932cfb79d93f7b984865e22cdee02c27ec40858
mharris162 <mharris162@student.alamo.edu>
2024-10-15 18:23:35 -0500

    up to 33:10 in vid
    computer choice section done

--- app.js
+++ app.js
@@ -8,15 +8,37 @@ const game = () => {
         const match = document.querySelector('.match');
 
         playBtn.addEventListener('click', () => {
-        introScreen.classList.add('fadeOut');
-        match.classList.add('fadeIn');
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
     });
 
 };
 
+//play match
+
+const playMatch = () => {
+    const options = document.querySelectorAll('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+
+    //computer options
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+    options.forEach(options => {
+        options.addEventListener('click', function(){
+            /// computer choice
+            const computerNumber = Math.floor(Math.random() * 3);
+            const computerChoice = computerOptions[computerNumber];
+        });
+    });
+
+    const computerNumber = Math.floor(Math.random() * 3);
+    computerOptions[computerNumber]
+};
+
 //is call all the inner function
 startGame();
-
+playMatch();
 
 };
 
