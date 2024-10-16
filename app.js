22578ffe5c9a45c21df490746cf2ac66a88a519a
mharris162 <mharris162@student.alamo.edu>
2024-10-15 19:01:29 -0500

    up to 43:00 in vid
    text change not working; player hand disappears when clicking buttons

--- app.js
+++ app.js
@@ -17,24 +17,77 @@ const game = () => {
 //play match
 
 const playMatch = () => {
-    const options = document.querySelectorAll('.options button');
-    const playerHand = document.querySelector('.player-hand');
-    const computerHand = document.querySelector('.computer-hand');
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
 
     //computer options
-    const computerOptions = ['rock', 'paper', 'scissors'];
+    const computerOptions = ["rock", "paper", "scissors"];
 
     options.forEach(options => {
-        options.addEventListener('click', function(){
+        options.addEventListener("click", function() {
             /// computer choice
             const computerNumber = Math.floor(Math.random() * 3);
             const computerChoice = computerOptions[computerNumber];
+
+             compareHands(this.textContent, computerChoice);
+
+            //update images
+            playerHand.src = `./assets/${this.textContent}.png`; ///something wrong with code- makes player hands disappear
+            computerHand.src = `./assets/${computerChoice}.png`;
         });
     });
 
-    const computerNumber = Math.floor(Math.random() * 3);
-    computerOptions[computerNumber]
 };
+
+ // where we call to compare hands 
+
+const compareHands = (playerChoice, computerChoice) =>{
+    //update text
+    const winner = document.querySelector('.winner');
+
+    //text for a tie
+    if (playerChoice === computerChoice){
+        winner.textContent = 'It is a tie';
+        return;
+    }
+
+    //text for rock
+
+    if(playerChoice === 'rock'){
+        if(computerChoice === 'scissors'){
+            winner.textContent = 'Player Wins';
+            return;
+        }else{
+            winner.textContent = 'Computer Wins';
+            return;
+        } 
+}
+// text for paper
+
+    if(playerChoice === 'paper'){
+        if(computerChoice === 'scissors'){
+            winner.textContent = 'Computer Wins';
+            return;
+        }else{
+            winner.textContent = 'Player Wins';
+            return;
+        } 
+    }
+
+// text for scissors
+
+if(playerChoice === 'scissors'){
+        if(computerChoice === 'paper'){
+            winner.textContent = 'Player Wins';
+            return;
+        }else{
+            winner.textContent = 'Computer Wins';
+            return;
+        } 
+    }
+
+}
 
 //is call all the inner function
 startGame();
